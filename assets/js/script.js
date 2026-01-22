6bdea311098acf07343c3e115eaa2e15254bf66d
GiorgiaFormicola <giorgia.formicola97@gmail.com>
2026-01-22 17:46:16 +0100

    Cards layout update

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,36 @@ const APIkey = "0VmFaORh2fcZ7J2rf5cLewQnaYkwa97br8QPHSxxWCq6Kk6tFnE5qfHq"; //Aut
 
 const pexelsURL = "https://api.pexels.com/v1/search?query="; // Pexels generic search URL
 
+// modifies PLACEHOLDERS CARDS for a better layout
+const allCards = document.querySelectorAll(".album .col-md-4");
+const allCardsTitles = document.querySelectorAll(".album .col-md-4 h5");
+const allCardBadges = document.querySelectorAll("small");
+const allCardsBodies = document.querySelectorAll(".card-body");
+const allCardParagraphs = document.querySelectorAll(".album .col-md-4 p");
+
+allCards.forEach((card, i) => {
+  card.classList.add("d-flex");
+
+  const cardTitle = allCardsTitles[i];
+  cardTitle.innerText = `Photo ${i + 1}`;
+
+  const cardBadge = allCardBadges[i];
+  cardBadge.innerText = `ID ...`;
+
+  const cardBody = allCardsBodies[i];
+  cardBody.classList.add("d-flex", "flex-column");
+
+  const cardParagraph = allCardParagraphs[i];
+  cardParagraph.classList.add("flex-grow-1");
+});
+
+// create a function that displays all cards again after being hidden
+const displayAllCards = function () {
+  allCards.forEach((card) => {
+    card.classList.remove("d-none");
+  });
+};
+
 // GET request to Pexels to substitute images
 const getImages = function (string) {
   fetch(pexelsURL + string, {
@@ -21,18 +51,16 @@ const getImages = function (string) {
 
       const photosArray = album.photos;
       const allCardImages = document.querySelectorAll("img");
-      const allCardBadges = document.querySelectorAll("small");
-      const allCardParagraphs = document.querySelectorAll(".album .col-md-4 p");
 
       photosArray.forEach((photo, i) => {
         if (i < 9) {
           const photoSRC = photo.src;
           const landscapeSRC = photoSRC.landscape;
           const cardImage = allCardImages[i];
-          cardImage.setAttribute("src", landscapeSRC);
+          cardImage.src = landscapeSRC;
 
           const photoDescription = photo.alt;
-          cardImage.setAttribute("alt", photoDescription);
+          cardImage.alt = photoDescription;
 
           const photoID = photo.id;
           const cardBadge = allCardBadges[i];
@@ -53,16 +81,17 @@ const getImages = function (string) {
 const primaryLoadButton = document.getElementById("primaryLoadButton");
 primaryLoadButton.addEventListener("click", () => {
   getImages("dolphins");
+  displayAllCards();
 });
 
 const secondaryLoadButton = document.getElementById("secondaryLoadButton");
 secondaryLoadButton.addEventListener("click", () => {
   getImages("tigers");
+  displayAllCards();
 });
 
 // change EDIT button to HIDE button and make it active
 const allEditButtons = document.querySelectorAll(".card button:nth-of-type(2)");
-const allCards = document.querySelectorAll(".album .col-md-4");
 
 allEditButtons.forEach((button, i) => {
   button.innerText = "Hide";
@@ -80,16 +109,8 @@ form.addEventListener("submit", function (e) {
   e.preventDefault();
   const searchInput = document.getElementById("search").value;
   getImages(searchInput);
+  displayAllCards();
 });
 
-// change CARD TITLE inner text
-const allCardsTitles = document.querySelectorAll(".album .col-md-4 h5");
-const allCardBadges = document.querySelectorAll("small");
-
-allCards.forEach((title, i) => {
-  const cardTitle = allCardsTitles[i];
-  cardTitle.innerText = `Photo ${i + 1}`;
-
-  const cardBadge = allCardBadges[i];
-  cardBadge.innerText = `ID ...`;
-});
+// start browser with random images
+getImages("random");
